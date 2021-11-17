2d0297df3d163e9301bd500d771425f0bb993490
Abdelrahman Alshater <shater_11@hotmail.com>
2021-11-16 22:15:54 -0300

    did the seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Seeding'
+
+10.times do
+  article = Article.new(
+    title: Faker::Book.title,
+    content: Faker::Lorem
+  )
+  article.save!
+  puts "#{article.title} saved to database"
+end
+
+puts 'Done!'
